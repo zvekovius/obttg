d04a5731d5797bfa5d2a8a00787ad38bbad86fe4
zvekovius <zachvek@gmail.com>
2016-12-13 20:12:01 -0600

    Functional test additions: We can now test for a header element and input + an actual list.

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import unittest
 
@@ -15,25 +16,34 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
+		#User notices page title includes to-do.
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
-#user enters to-do item. 
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
-#user types "Buy peacock feathers"
+		#user is invited to enter a to-do item.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
-#user hits enter and "1: Buy peackcock feather" is the first item in to do list.
+		#user enters 'buy peacock feathers as a todo item.
+		inputbox.send_keys('Buy peacock feathers')
 
-#Still another text box to enter new items. 
+		#user hits enter.
+		input.send_keys(Keys.ENTER)
 
-#User enters "Use Peacock feathers to make a fly"
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1. Buy peacock feathers' for row in rows)
+		)
 
-#The page updates again after hitting enter. 
+		#User wants to add another to-do item. 
+		self.fail('finish the test!')
 
-#User looks to URL for unique identifier to save the list. 
-
-#User goes to that URL, the to-do list is still there (2 items). 
-
-#User quits. 
+		#The page updates again, and now shows both items on her list.
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
