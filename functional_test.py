53e08565b742b31813ae435eaaf01f7370213efa
zvekovius <zachvek@gmail.com>
2016-12-13 20:34:17 -0600

    Fix up functional test error. Front page template changes

--- functional_test.py
+++ functional_test.py
@@ -32,12 +32,13 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox.send_keys('Buy peacock feathers')
 
 		#user hits enter.
-		input.send_keys(Keys.ENTER)
+		inputbox.send_keys(Keys.ENTER)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1. Buy peacock feathers' for row in rows)
+			any(row.text == '1. Buy peacock feathers' for row in rows),
+			"New to-do item did not appear in the table"
 		)
 
 		#User wants to add another to-do item. 
