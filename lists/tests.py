7d109312bea23d03800896b0e4582b23980a2f5a
zvekovius <zachvek@gmail.com>
2016-12-13 19:44:37 -0600

    Passing unit test. Very basic web page!

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
+from django.http import HttpRequest
 
 
 # Create your tests here.
@@ -11,3 +12,10 @@ class HomePageTest(TestCase):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
 
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		html = response.content.decode('utf8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do lists</title>',html)
+		self.assertTrue(html.endswith('</html>'))
