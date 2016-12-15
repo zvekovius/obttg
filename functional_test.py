2d0c094180ae3a5bcd438cf6a7e2a1fdadcd34cb
zvekovius <zachvek@gmail.com>
2016-12-14 20:25:58 -0600

    Changes to functional tests

--- functional_test.py
+++ functional_test.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
 
@@ -30,13 +35,13 @@ class NewVisitorTest(unittest.TestCase):
 
 
 		inputbox.send_keys('Buy peacock feathers')
-
-		#user hits enter.
 		inputbox.send_keys(Keys.ENTER)
-		
+		self.check_for_row_in_list_table('1: Buy peacock feathers')		
+
+
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		#user enters 'buy peacock feathers as a todo item.
-		inputbox.send_keys('Buy more peacock feathers.')
+		inputbox.send_keys('Buy more peacock feathers')
 
 		#user hits enter.
 		inputbox.send_keys(Keys.ENTER)
@@ -44,8 +49,8 @@ class NewVisitorTest(unittest.TestCase):
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-		self.assertIn('2: Buy more peacock feathers.',[row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')	
+		self.check_for_row_in_list_table('2: Buy more peacock feathers')	
 
 
 
