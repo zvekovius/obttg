8bcbd32f5ea1d199b15ece30657276f16e4820a0
zvekovius <zachvek@gmail.com>
2016-12-17 21:47:12 -0600

    New URL + view for adding to existing lists. Functional test finally passes. Before refactor on page 111

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -77,8 +77,4 @@ class NewVisitorTest(LiveServerTestCase):
 
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertIn('Buy milk', page_text)
- 
-		self.fail('finish the test!')
-
-
+		self.assertIn('Buy Milk', page_text)
